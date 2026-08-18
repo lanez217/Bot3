af46129cc189c2c9a350742999ade0959c959e28
lanez217 <alhassancariba@gmail.com>
2026-08-18 10:07:25 +0000

    Specify Node.js engine version requirement
    Added Node.js engine requirement to package.json.

--- package.js
+++ package.js
@@ -6,8 +6,12 @@
   "scripts": {
     "start": "node server.js"
   },
+  "engines": {
+    "node": ">=20.0.0"
+  },
   "dependencies": {
     "@whiskeysockets/baileys": "^6.7.18",
+    "dotenv": "^16.4.5",
     "express": "^4.19.2",
     "pino": "^9.0.0",
     "socket.io": "^4.7.5",
